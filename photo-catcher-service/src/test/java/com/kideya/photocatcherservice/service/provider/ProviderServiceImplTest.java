adaf99a4c36c00d828abdf4013da630704a5fe0a
Koekin Yaroslav <ykoekin@gmail.com>
2021-09-05 17:58:56 +0300

    Add test cases

--- photo-catcher-service/src/test/java/com/kideya/photocatcherservice/service/provider/ProviderServiceImplTest.java
+++ photo-catcher-service/src/test/java/com/kideya/photocatcherservice/service/provider/ProviderServiceImplTest.java
@@ -32,10 +32,12 @@ class ProviderServiceImplTest {
     @BeforeEach
     void setUp() {
         Image image1 = createImage("image1", 1, 2, List.of(new ImageTag("tag1")), LocalDate.of(2021, 1, 1));
-        Image image2 = createImage("image2", 1, 2, List.of(), LocalDate.of(2019, 1, 1));
+        Image image2 = createImage("image2", 1, 2, List.of(new ImageTag("tag1"), new ImageTag("tag2")), LocalDate.of(2019, 1, 1));
         Image image3 = createImage("image3", 1, 3, List.of(), LocalDate.of(2021, 2, 1));
+        Image image4 = createImage("image4", 2, 3, List.of(new ImageTag("tag1")), LocalDate.of(2010, 1, 1));
 
         Mockito.when(imageRepository.findByUserId(Mockito.eq(1))).thenReturn(List.of(image1, image2, image3));
+        Mockito.when(imageRepository.findByUserId(Mockito.eq(2))).thenReturn(List.of(image4));
 
         providerService = new ProviderServiceImpl(imageRepository);
 
@@ -43,23 +45,33 @@ class ProviderServiceImplTest {
 
     @Test
     void findByGroupId() {
-        ByGroupIdFinder byGroupIdFinder = new ByGroupIdFinder(1, 2);
+        ByGroupIdFinder byGroupIdFinder1 = new ByGroupIdFinder(1, 2);
+        ByGroupIdFinder byGroupIdFinder2 = new ByGroupIdFinder(1, 3);
+        ByGroupIdFinder byGroupIdFinder3 = new ByGroupIdFinder(2, 3);
 
-        assertEquals(2, providerService.find(byGroupIdFinder).size());
+        assertEquals(2, providerService.find(byGroupIdFinder1).size());
+        assertEquals(1, providerService.find(byGroupIdFinder2).size());
+        assertEquals(1, providerService.find(byGroupIdFinder3).size());
     }
 
     @Test
     void findByTag() {
-        ByTagFinder byTagFinder = new ByTagFinder(1, "tag1");
+        ByTagFinder byTagFinder1 = new ByTagFinder(1, "tag1");
+        ByTagFinder byTagFinder2 = new ByTagFinder(1, "tag2");
+        ByTagFinder byTagFinder3 = new ByTagFinder(2, "tag1");
 
-        assertEquals(1, providerService.find(byTagFinder).size());
+        assertEquals(2, providerService.find(byTagFinder1).size());
+        assertEquals(1, providerService.find(byTagFinder2).size());
+        assertEquals(1, providerService.find(byTagFinder3).size());
     }
 
     @Test
     void findByDate() {
-        ByDateFinder byDateFinder = new ByDateFinder(1, LocalDate.of(2020, 1, 1), LocalDate.now());
+        ByDateFinder byDateFinder1 = new ByDateFinder(1, LocalDate.of(2020, 1, 1), LocalDate.of(2021, 10, 1));
+        ByDateFinder byDateFinder2 = new ByDateFinder(2, LocalDate.of(2020, 1, 1), LocalDate.of(2021, 10, 1));
 
-        assertEquals(2, providerService.find(byDateFinder).size());
+        assertEquals(2, providerService.find(byDateFinder1).size());
+        assertEquals(0, providerService.find(byDateFinder2).size());
     }
 
 }
