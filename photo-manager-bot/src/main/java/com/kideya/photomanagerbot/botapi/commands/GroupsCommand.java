0e1a0357db2ce6e83b72a5287db74df2bbf7847a
Irina Tsekhanovich <irina.tsekhanovich@softline.com>
2021-09-07 18:01:35 +0300

    get groups endpoint

--- photo-manager-bot/src/main/java/com/kideya/photomanagerbot/botapi/commands/GroupsCommand.java
+++ photo-manager-bot/src/main/java/com/kideya/photomanagerbot/botapi/commands/GroupsCommand.java
@@ -1,11 +1,15 @@
 package com.kideya.photomanagerbot.botapi.commands;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
+import com.kideya.photomanagerbot.model.settings_service.GroupsDto;
+import com.kideya.photomanagerbot.services.LocaleTextService;
+import com.kideya.photomanagerbot.services.SendingMessageService;
+import com.kideya.photomanagerbot.services.TelegramApiSendingService;
+import com.kideya.photomanagerbot.utils.MicroservicesNames;
+import com.kideya.photomanagerbot.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
@@ -15,21 +19,27 @@ public class GroupsCommand implements BotCommand {
     private final String name = "/groups";
 
     @Autowired
-    private EurekaClient eurekaClient;
+    private SendingMessageService sendingMessageService;
+
+    @Autowired
+    private TelegramApiSendingService telegramApiSendingService;
+
+    @Autowired
+    private LocaleTextService localeService;
 
     @Override
     public BotApiMethod<?> runCommand(Update update) {
-        InstanceInfo instanceInfo = eurekaClient.getApplications().getInstancesByVirtualHostName("TEST").get(0);
+        Integer userId = Utils.getUserId(update);
+        String groupsUrl = "/api/settings/users/"+userId+"/groups";
+        ResponseEntity<GroupsDto> groupsDtoEntity = sendingMessageService.sendGet(MicroservicesNames.SETTINGS_SERVICE_NAME,
+                groupsUrl, GroupsDto.class);
 
-        RestTemplate restTemplate = new RestTemplateBuilder().build();
+        Long chatId = Utils.getChatId(update);
+        String textMessage = localeService.getTranslatedText("groups.get_groups") + "\n" +
+                groupsDtoEntity.getBody().getGroupsString();
+        telegramApiSendingService.sendTextMessage(chatId, textMessage);
+        return new AnswerCallbackQuery();
 
-        String url = "http://" + instanceInfo.getHostName() +":"+instanceInfo.getPort() +"/test";
-        System.out.println(url);
-        restTemplate.getForEntity(url, String.class);
-//        restTemplate.getForEntity("http://TEST:5555/test", String.class);
-//        Integer answer = restTemplate.getForObject("url", int.class);
-//        System.out.println(12);
-        return null;
     }
 
     @Override
